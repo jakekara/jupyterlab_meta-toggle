e8bb0bef13539625c5d7bf56446b0cd3c8b764e5
Jake Kara <jake@jakekara.com>
2020-09-03 15:49:43 -0400

    prettify

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,20 @@
-import {
-  IDisposable, DisposableDelegate
-} from '@lumino/disposable';
+import { IDisposable, DisposableDelegate } from '@lumino/disposable';
 
 import {
-  JupyterFrontEnd, JupyterFrontEndPlugin
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
-import {
-  ToolbarButton
-} from '@jupyterlab/apputils';
+import { ToolbarButton } from '@jupyterlab/apputils';
+
+import { DocumentRegistry } from '@jupyterlab/docregistry';
 
 import {
-  DocumentRegistry
-} from '@jupyterlab/docregistry';
-
-import {
-  // NotebookActions, 
-  NotebookPanel, INotebookModel, INotebookTracker
+  // NotebookActions,
+  NotebookPanel,
+  INotebookModel,
+  INotebookTracker
 } from '@jupyterlab/notebook';
-
 
 /**
  * The plugin registration information.
@@ -26,41 +22,44 @@ import {
 const plugin: JupyterFrontEndPlugin<void> = {
   activate,
   id: 'meta-toggle',
-  requires:[INotebookTracker],
+  requires: [INotebookTracker],
   autoStart: true
 };
-
 
 /**
  * A notebook widget extension that adds a button to the toolbar.
  */
-export
-class ButtonExtension implements DocumentRegistry.IWidgetExtension<NotebookPanel, INotebookModel> {
-
+export class ButtonExtension
+  implements DocumentRegistry.IWidgetExtension<NotebookPanel, INotebookModel> {
   tracker: INotebookTracker;
 
-  constructor(tracker:INotebookTracker){
+  constructor(tracker: INotebookTracker) {
     this.tracker = tracker;
   }
   /**
    * Create a new extension object.
    */
-  createNew(panel: NotebookPanel, context: DocumentRegistry.IContext<INotebookModel>): IDisposable {
-    let callback = () => {
+  createNew(
+    panel: NotebookPanel,
+    context: DocumentRegistry.IContext<INotebookModel>
+  ): IDisposable {
+    const callback = (): void => {
       // NotebookActions.runAll(panel.content, context.sessionContext);
       console.log('active cell', this.tracker.activeCell);
       console.log(
         'active cell metadata',
         this.tracker.activeCell.model.metadata
       );
-      const key = "meta-toggle";
+      const key = 'meta-toggle';
       const cellMeta = this.tracker.activeCell.model.metadata;
       const value = cellMeta.get(key);
       let newValue = false;
-      if (value === false){ newValue = true}
-      cellMeta.set(key, newValue)
+      if (value === false) {
+        newValue = true;
+      }
+      cellMeta.set(key, newValue);
     };
-    let button = new ToolbarButton({
+    const button = new ToolbarButton({
       className: 'myButton',
       iconClass: 'fa fa-fast-forward',
       onClick: callback,
@@ -77,11 +76,10 @@ class ButtonExtension implements DocumentRegistry.IWidgetExtension<NotebookPanel
 /**
  * Activate the extension.
  */
-function activate(app: JupyterFrontEnd, tracker:INotebookTracker) {
-  console.log("tracker", tracker)
+function activate(app: JupyterFrontEnd, tracker: INotebookTracker): void {
+  console.log('tracker', tracker);
   app.docRegistry.addWidgetExtension('Notebook', new ButtonExtension(tracker));
-};
-
+}
 
 /**
  * Export the plugin as default.
